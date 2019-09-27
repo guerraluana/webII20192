2cd6faac92df15db13f71b6c159552e67263db54
Francielle Mendes <franciellefmendes@gmail.com>
2019-09-27 13:20:25 -0300

    Exercício 2

---
+++ Exercicios/src/java/ex2/Usuario.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex2;
+
+/**
+ *
+ * @author francielle.ti
+ */
+class Usuario {
+    String nome;
+    String login;
+    String senha;
+
+    public Usuario(String nome, String login, String senha) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
