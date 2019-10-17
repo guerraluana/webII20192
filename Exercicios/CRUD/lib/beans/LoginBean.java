01cd068b76676cfdecbc45b5bf5de6e1f95d6950
Matheus <97kemer@gmail.com>
2019-10-17 20:20:53 -0300

    more files

---
+++ Exercicios/CRUD/lib/beans/LoginBean.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author matheus
+ */
+public class LoginBean implements Serializable {
+    public int id;
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
